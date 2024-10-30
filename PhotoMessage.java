c121b47737c688210a758efdbfd29738393cbcd0
jebba <jeb.bazzell@gmail.com>
2024-10-30 10:07:42 -0400

    added files

---
+++ PhotoMessage.java
@@ -0,0 +1,13 @@
+import java.time.LocalDateTime;
+
+public class PhotoMessage implements Message {
+
+    public Object getMessage() {
+        return null;
+    }
+
+    @Override
+    public LocalDateTime getTimeStamp() {
+        return null;
+    }
+}
---
+++ TextMessage.java
@@ -0,0 +1,13 @@
+import java.time.LocalDateTime;
+
+public class TextMessage implements Message {
+
+    public Object getMessage() {
+        return null;
+    }
+
+    @Override
+    public LocalDateTime getTimeStamp() {
+        return null;
+    }
+}
