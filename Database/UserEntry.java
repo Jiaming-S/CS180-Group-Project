71e53a5446bc0af869c434c77eb96514eac1a486
JMST <jiamingsitu06@gmail.com>
2024-11-16 15:27:54 -0500

    Update UserEntry.java

--- Database/UserEntry.java
+++ Database/UserEntry.java
@@ -35,7 +35,7 @@ public class UserEntry extends GenericEntry {
 
   @Override
   protected void handleXML(String content, String curTag, String parentTag) {
-    if (curTag.equals("ID") && parentTag.equals("Message")) {
+    if (curTag.equals("ID") && parentTag.equals("User")) {
       this.ID = Integer.parseInt(content);
     } else if (curTag.equals("Username")) {
       this.username = content;
