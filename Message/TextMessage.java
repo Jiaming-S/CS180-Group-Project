66ef9ec1485da03f1c4cd64696ee962185e7f744
jebba <jeb.bazzell@gmail.com>
2024-11-17 22:56:58 -0500

    Simple textmessage implementation

--- Message/TextMessage.java
+++ Message/TextMessage.java
@@ -1,15 +1,24 @@
 package Message;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class TextMessage implements Message {
 
-    public Object getMessage() {
-        return null;
+    private String message;
+    private LocalDateTime timeStamp;
+
+    public TextMessage(String message, LocalDateTime timeStamp) {
+        this.message = message;
+        this.timeStamp = timeStamp;
+    }
+
+    public String getMessage() {
+        return message;
     }
 
     @Override
     public LocalDateTime getTimeStamp() {
-        return null;
+        return timeStamp;
     }
 }
