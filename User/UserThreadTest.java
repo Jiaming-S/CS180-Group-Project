03993b54bffeb137911d2bf690c0435a0e48ed17
RFeng12 <feng426@purdue.edu>
2024-11-17 20:58:56 -0500

    Updated UserThreadTest

--- User/UserThreadTest.java
+++ User/UserThreadTest.java
@@ -9,6 +9,8 @@ import Database.*;
 import java.net.*;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -30,11 +32,11 @@ public class UserThreadTest {
 
 
         String hostName = "localhost";
-        int portUDBS = 5943;
-        int portMDBS = 4383;
-        Socket userSocket, messageSocket;
-        ObjectOutputStream uoos, moos;
-        ObjectInputStream uois, mois;
+        int portUDBS = 3333;
+        int portMDBS = 4444;
+        Socket socket, messageSocket;
+        ObjectOutputStream oos, moos;
+        ObjectInputStream ois, mois;
 
         try {
             userDatabase = new UserDatabase("user_db.txt");
@@ -55,47 +57,48 @@ public class UserThreadTest {
             Thread mdbs = new Thread(messageDatabaseServer);
             mdbs.start(); // note: probably don't need to do anything with message db in runner, good to just start it tho
 
-            userSocket = new Socket(hostName, portUDBS);
-            uoos = new ObjectOutputStream(userSocket.getOutputStream());
-            uois = new ObjectInputStream(userSocket.getInputStream());
+            socket = new Socket(hostName, portUDBS);
+            oos = new ObjectOutputStream(socket.getOutputStream());
+            ois = new ObjectInputStream(socket.getInputStream());
 
             messageSocket = new Socket(hostName, portMDBS);
-            moos = new ObjectOutputStream(messageSocket.getOutputStream());
-            mois = new ObjectInputStream(messageSocket.getInputStream());
+            moos = new ObjectOutputStream(socket.getOutputStream());
+            mois = new ObjectInputStream(socket.getInputStream());
         } catch (IOException e) {
             e.printStackTrace();
             throw e;
         }
 
         String newUserStr = """
-            <User>
-                <Username>name4</Username>
-                <Password>password123</Password>
-                <ID>00000004</ID>
-                <FriendList>
-                    <ID>11223344</ID>
-                    <ID>55667788</ID>
-                    <ID>12345678</ID>
-                </FriendList>
-                <BlockList>
-                    <ID>10101010</ID>
-                    <ID>44444444</ID>
-                </BlockList>
-                <ProfilePicture>/path/to/image.png</ProfilePicture>
-                <Region>USA/Midwest</Region>
-            </User>""".replaceAll(" ", "").replaceAll("\n", "");
+        <User>
+            <Username>name4</Username>
+            <Password>password123</Password>
+            <ID>00000004</ID>
+            <FriendList>
+                <ID>11223344</ID>
+                <ID>55667788</ID>
+                <ID>12345678</ID>
+            </FriendList>
+            <BlockList>
+                <ID>10101010</ID>
+                <ID>44444444</ID>
+            </BlockList>
+            <ProfilePicture>/path/to/image.png</ProfilePicture>
+            <Region>USA/Midwest</Region>
+        </User>""".replaceAll(" ", "").replaceAll("\n", "");
 
         User tempUser = new User(new UserEntry(newUserStr));
 
-        userThread = new UserThread(tempUser, userSocket, messageSocket);
-
+        userThread = new UserThread(tempUser, new Socket("localhost", portUDBS), new Socket("localhost", portMDBS));
+                    
         if (userThread != null) {
             userThread.start();
         } try {
             if (userThread != null) userThread.join();
-            if (uois != null) uois.close();
-            if (uoos != null) uoos.close();
-            if (userSocket != null) userSocket.close();
+            if (ois != null) ois.close();
+            if (oos != null) oos.close();
+            if (socket != null) socket.close();
+
             if (mois != null) mois.close();
             if (moos != null) moos.close();
             if (messageSocket != null) messageSocket.close();
